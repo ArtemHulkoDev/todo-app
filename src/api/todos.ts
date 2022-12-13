965d0feda7b3abf1208cb3d102a97f60b7433ef2
Artem Hulko <agulko96@gmail.com>
2022-12-13 11:33:02 +0200

    created TODO app

---
+++ src/api/todos.ts
@@ -0,0 +1,18 @@
+import { Todo } from '../types/Todo';
+import { client } from '../utils/fetchClient';
+
+export const getTodos = (userId: number) => {
+  return client.get<Todo[]>(`/todos?userId=${userId}`);
+};
+
+export const addTodo = (todo: Partial<Todo>) => {
+  return client.post<Todo>('/todos', todo);
+};
+
+export const removeTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const changeTodo = (todoProp: Partial<Todo>, todoId: number) => {
+  return client.patch(`/todos/${todoId}`, todoProp);
+};
